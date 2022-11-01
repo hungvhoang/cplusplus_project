0c417206474d8d28b0d7c8315c4cf1ea2f18af81
Hung Hoang <hunghvse183800@fpt.edu.vn>
2022-11-01 22:20:15 +0700

    change field  name

---
+++ ex55.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	double r,p,t,ci,ta;
+	cout<< "Input the principle: ";
+	cin>> p;
+	cout<<"Input the rate of interest: ";
+	cin>> r;
+	cout<<"Input the time: ";
+	cin>> t;
+	ta=p*pow((1+r);
+}
