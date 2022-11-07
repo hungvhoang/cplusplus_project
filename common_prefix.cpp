234feea4afe3b6cdf4125f4607a90126e0d898a3
Hung Hoang <hunghvse183800@fpt.edu.vn>
2022-11-07 23:24:24 +0700

    done

--- common_prefix.cpp
+++ common_prefix.cpp
@@ -5,16 +5,21 @@
 
 using namespace std;
 string commonprefix(vector<string> str) {
-	string prefix= str[0];
-	for (int i=0; i< str.size();++i) {
-		string s = str[i];
-		for (int j = 0; j < s.size(); ++j) {
-			if (  s  != prefix  )	
-			prefix = prefix.substr(0, j+1 );
+	string prefix;
+	string first = str[0];
+	for (int i=0; i< first.size();++i) {
+		char a= first[i];
+		for (int j = 1; j < str.size(); ++j) {
+			if (  str[j][i]  != a  )
+			break;	
+			if (i != 0) {
+				for (int k =0;k <= i-1; k++){
+				prefix=first.substr(0,k);
+				}
 			break;
 		}
 	}
-
+	}
 
 	return prefix;
 }
